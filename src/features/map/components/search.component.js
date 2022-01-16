a5a88f331c097ec1a72871d010d822f0ab88f68e
jjxmonster <kuba.tarabasz@wp.pl>
2022-01-16 22:14:54 +0100

    search bar in map screen

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -1,9 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react';
-
+import styled from 'styled-components';
 import { Searchbar } from 'react-native-paper';
-import { SearchBarWrapper } from '../screens/RestaurantsScreen/restaurants.screen.styles.js';
 
 import { LocationContext } from '../../../services/location/location.context.js';
+
+export const SearchBarWrapper = styled.View`
+   flex: 1;
+   width: 100%;
+   justify-content: center;
+   padding-left: ${({ theme }) => theme.sizes[1]};
+   padding-right: ${({ theme }) => theme.sizes[1]};
+   min-height: 48px;
+   position: absolute;
+   top: 50px;
+   z-index: 999;
+`;
 
 const Search = () => {
    const { keyword, search } = useContext(LocationContext);
@@ -16,6 +27,7 @@ const Search = () => {
    return (
       <SearchBarWrapper>
          <Searchbar
+            icon='map'
             placeholder='Search for a location'
             value={searchKeyword}
             onSubmitEditing={() => {
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
-import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components';
 
-const Map = () => {
+import Search from '../components/search.component.js';
+
+import { LocationContext } from '../../../services/location/location.context.js';
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context.js';
+
+const Map = styled(MapView)`
+   height: 100%;
+   width: 100%;
+`;
+
+const MapScreen = () => {
+   const { location } = useContext(LocationContext);
+   const { restaurants = [] } = useContext(RestaurantContext);
+
+   const [latDelta, setLatDelta] = useState();
+
+   const [viewport] = location;
+
+   useEffect(() => {
+      const northeastLat = viewport.northeast.lat;
+      const southweastLat = viewportsouthweast.lat;
+   }, [location]);
+
    return (
-      <SafeAreaView>
-         <MapView style={{ height: '100%' }} />
-      </SafeAreaView>
+      <>
+         <Search />
+         <Map>
+            {restaurants.map(restaurant => {
+               return null;
+            })}
+         </Map>
+      </>
    );
 };
 
-export default Map;
+export default MapScreen;
