eb3a46b0e83243a6b9766f1389115f66a9b35902
jjxmonster <kuba.tarabasz@wp.pl>
2022-02-07 22:24:40 +0100

    install and configure firebase

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { initializeApp, getApps } from 'firebase/app';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context.js';
 import { LocationContextProvider } from './src/services/location/location.context.js';
@@ -18,13 +20,34 @@ import { theme } from './src/infrastructure/theme';
 
 import AppNavigator from './src/infrastructure/navigation/app.navigator.js';
 
+const firebaseConfig = {
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: 'meals-to-go-4d722.firebaseapp.com',
+   projectId: 'meals-to-go-4d722',
+   storageBucket: 'meals-to-go-4d722.appspot.com',
+   messagingSenderId: '157312654518',
+   appId: '1:157312654518:web:9dc40e990637fd7c069a17',
+};
+if (!getApps().length) initializeApp(firebaseConfig);
+
 export default function App() {
+   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
    const [oswaldLoaded] = useOswald({
       Oswald_400Regular,
    });
    const [latoLoaded] = useLato({
       Lato_400Regular,
    });
+
+   useEffect(() => {
+      signInWithEmailAndPassword('email@binni.io', 'password')
+         .then(user => {
+            console.log(user.email);
+            setIsAuthenticated(true);
+         })
+         .catch(err => console.log(err));
+   }, []);
 
    if (!oswaldLoaded || !latoLoaded) {
       return null;
